643d64bc9ab0a749aa2d72b6816160e07c15c2a2
Korvenius <kevin.b.okelly@gmail.com>
2018-09-23 23:48:00 +0100

    Simple anagram solver added

---
+++ AnagramSolver.java
@@ -0,0 +1,41 @@
+//very basic anagram solver. only works with single word anagrams.
+
+import java.util.*;
+import java.io.*;
+
+public class AnagramSolver{
+
+	public static void main(String[] args) throws IOException{
+
+		//Scanner for user to input anagram to be solved		
+		Scanner inScan = new Scanner(System.in);
+		//Output prompt for user		
+		System.out.println("Input anagram to be solved:");		
+		String strA = inScan.next();
+		//Make an array the length of the input string
+		char[] arrA = new char[strA.length()];
+		//Populate the array with the characters of input string
+		for(int i=0; i<arrA.length; i++){
+			arrA[i]=strA.charAt(i);		
+		}
+		//Sort the characters alphabetically
+		Arrays.sort(arrA);
+	
+		//Scanner to scan through a list of English language words
+		Scanner txtScan = new Scanner(new File("wordlist.txt"));
+		while(txtScan.hasNextLine()){
+			String strB=txtScan.nextLine();	
+			char[] arrB = new char[strB.length()];
+			for(int i=0; i<arrB.length; i++){
+				arrB[i]=strB.charAt(i);		
+			}
+			Arrays.sort(arrB);
+			
+			if(Arrays.equals(arrA, arrB)){			
+				System.out.println(strB);		
+			}
+		}	
+
+	}
+
+}
