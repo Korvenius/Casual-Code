0d67636fe0cc65080d0d5b3d68b3b2e4d30789a8
Korvenius <kevin.b.okelly@gmail.com>
2018-09-24 13:37:38 +0100

    imports reduced, output more readable, unnecessary code removed

--- AnagramSolver.java
+++ AnagramSolver.java
@@ -1,8 +1,10 @@
 //very basic anagram solver. only works with single word anagrams.
 //made while watching 8 out of 10 cats does countdown.
 
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
 
 public class AnagramSolver{
 
@@ -12,26 +14,19 @@ public class AnagramSolver{
 		//Scanner for user to input anagram to be solved		
 		Scanner inScan = new Scanner(System.in);
 		//Output prompt for user		
-		System.out.println("Input anagram to be solved:");		
+		System.out.println("\nInput anagram to be solved:\n");		
 		String strA = inScan.next();
-		//Make an array the length of the input string
-		char[] arrA = new char[strA.length()];
-		//Populate the array with the characters of input string
-		for(int i=0; i<arrA.length; i++){
-			arrA[i]=strA.charAt(i);		
-		}
+        System.out.println("---------------------------");
+		//Create a character array of the input string
+		char[] arrA = strA.toCharArray();
 		//Sort the characters alphabetically
 		Arrays.sort(arrA);
-
 		int count=0;	
 		//Scanner to scan through a list of English language words
 		Scanner txtScan = new Scanner(new File("wordlist.txt"));
 		while(txtScan.hasNextLine()){
 			String strB=txtScan.nextLine();	
-			char[] arrB = new char[strB.length()];
-			for(int i=0; i<arrB.length; i++){
-				arrB[i]=strB.charAt(i);		
-			}
+			char[] arrB = strB.toCharArray();
 			Arrays.sort(arrB);
 		
 			if(Arrays.equals(arrA, arrB)&&!(strA.equals(strB))){			
@@ -39,7 +34,7 @@ public class AnagramSolver{
 				count++;	
 			}
 		}
-		System.out.println(count+" anagram(s) found.");
+		System.out.println("\n"+count+" anagram(s) found.\n");
 
 	}
 
